fbd1c724e440ff3bcaa65c36a92ef2d950bbc836
Matt Rundle <m.n.rundle@gmail.com>
2024-06-08 19:46:07 -0500

    rds: create db instance

--- cdk/cdk/cdk_stack.py
+++ cdk/cdk/cdk_stack.py
@@ -4,6 +4,7 @@ from aws_cdk import (
     aws_s3 as s3,
     aws_ec2 as ec2,
     # aws_sqs as sqs,
+    aws_rds as rds,
     RemovalPolicy,
     CfnTag,
     CfnOutput,
@@ -22,6 +23,9 @@ class CdkStack(Stack):
         #     self, "CdkQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+
+        self.BLOG_VPC_CIDR = "10.0.0.0/16"
+        self.MYSQL_PORT = 3306
 
         self.setup_s3()
         self.setup_ec2()
@@ -46,12 +50,17 @@ class CdkStack(Stack):
         # create vpc
         vpc = ec2.Vpc(self, "blog-host-vpc",
             max_azs=2,
-            ip_addresses=ec2.IpAddresses.cidr("10.0.0.0/16"),
+            ip_addresses=ec2.IpAddresses.cidr(self.BLOG_VPC_CIDR),
             vpc_name="blog-host-vpc",
             subnet_configuration=[
                 ec2.SubnetConfiguration(
                     name="blog-host-public-subnet",
                     subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24,
+                ),
+                ec2.SubnetConfiguration(
+                    name="blog-host-private-subnet",
+                    subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
                     cidr_mask=24,
                 )
             ],
@@ -82,9 +91,43 @@ class CdkStack(Stack):
             instance_type=ec2.InstanceType("t3.micro"),
             machine_image=ec2.MachineImage.latest_amazon_linux2023(),
             vpc=vpc,
+            vpc_subnets=ec2.SubnetSelection(
+                subnet_type = ec2.SubnetType.PUBLIC,
+            ),
             security_group=sg,
             associate_public_ip_address=True,
             key_name=keypair.key_name,
         )
 
         CfnOutput(self, "InstanceId", value=instance.instance_id)
+
+        #
+        # set up rds instance
+        #
+        db = rds.DatabaseInstance(
+            self,
+            "blog_database",
+            engine = rds.DatabaseInstanceEngine.MYSQL,
+            vpc = vpc,
+            vpc_subnets = ec2.SubnetSelection(
+                subnet_type = ec2.SubnetType.PRIVATE_ISOLATED,
+            ),
+            credentials = rds.Credentials.from_generated_secret("Admin"),
+            instance_type = ec2.InstanceType.of(
+                ec2.InstanceClass.BURSTABLE3,
+                ec2.InstanceSize.MICRO),
+            port = self.MYSQL_PORT,
+            allocated_storage = 80,
+            multi_az = False,
+            removal_policy = RemovalPolicy.DESTROY,
+            deletion_protection = False,
+            publicly_accessible = False,
+        )
+        # XXX/TODO use separate security group
+        db.connections.allow_from(
+            ec2.Peer.ipv4(self.BLOG_VPC_CIDR),
+            ec2.Port.tcp(self.MYSQL_PORT),
+            description="mysql db connection",
+        )
+        CfnOutput(self, "db_endpoint", value=db.db_instance_endpoint_address)
+
